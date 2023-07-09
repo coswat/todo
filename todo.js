4fe399e01cf5bad10d8bb8dd0e7193f9168a6ad1
coderswat <abhishek.b4696@gmail.com>
2023-07-09 17:51:04 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,60 @@
+const input = document.getElementById("inpTodo");
+const todoList = document.getElementById("todo-container");
+const btn = document.getElementById("btn-todo");
+const deleteAll = document.getElementById("rem-all");
+
+btn.addEventListener("click", (e) => {
+  addTodo();
+});
+
+deleteAll.addEventListener("click", (e) => {
+  removeAll();
+});
+
+todoList.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("completed");
+  }
+  if (e.target.tagName == "SPAN") {
+    removeTask(e);
+  }
+});
+
+function addTodo() {
+  if (input.value == "") {
+    alert("Please enter a task to add");
+  } else {
+    createTask(input.value);
+  }
+  input.value = "";
+}
+
+function createTask(task) {
+  // li node creation
+  let li = document.createElement("li");
+  li.innerText = task;
+  li.classList.add("pending");
+  // span node creation
+  let btn = document.createElement("span");
+  btn.innerHTML = "&#x2715;";
+  btn.classList.add("delete");
+  // appending
+  li.appendChild(btn);
+  todoList.appendChild(li);
+}
+
+function removeTask(node) {
+  let parentNode = node.target.parentNode;
+  parentNode.remove();
+}
+
+function removeAll() {
+  let allTask = document.querySelectorAll(".delete");
+  if (allTask.length < 1) {
+    alert("No task found to delete");
+  }
+  allTask.forEach((e) => {
+    let parentNode = e.parentNode;
+    parentNode.remove();
+  });
+}
